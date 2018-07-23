4ac37da85280849aa6f9806713c92e30e1c0723d
Krzysztof Ożegowski <krzysztof.ozegowski@mikronika.com.pl>
2018-07-23 18:24:06 +0200

    added webpack, react files and dependencies

---
+++ news-feeder-web/src/main/websrc/src/components/Main.js
@@ -0,0 +1,19 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
